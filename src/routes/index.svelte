6019b70b5a8a764f60f3aca4cd903b749b5cf878
Daniel Faustino <danieloliveirafaustino@gmail.com>
2022-05-16 21:46:06 -0300

    favicon, folder deleted

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
 	<title>Clínica F.Care</title>
 	<meta name="description" content="Clínica de Estética, que oferece serviços de forma responsável e qualificada. Cursos de Micropigmentação de Sobrancelhas, Extensão de Cílios, Design de Sobrancelhas e Revitalização Labial. Procedimentos: Extensão de Cílios, Epilação Egípcia, Estética, Peeling, Podologia, Despigmentação a Laser, Design de Sobrancelhas, Revitalização Labial, Micropigmentação, Extensão de Cílios, Botox, Preenchimento Labial.">
   <meta name="robots" content="index, follow">
-  <meta name="googlebot" content="Extensão de Cílios, Epilação Egípcia, Estética, Peeling, Podologia, Despigmentação a Laser, Design de Sobrancelhas, Revitalização Labial, Micropigmentação, Extensão de Cílios, Cursos,Botox,Preenchimento Labial" />
+  <meta name="googlebot" content="Extensão de Cílios, Epilação Egípcia, Estética, Peeling, Podologia, Despigmentação a Laser, Design de Sobrancelhas, Revitalização Labial, Micropigmentação, Extensão de Cílios, Cursos,Botox,Preenchimento Labial,Fabrine Macedo" />
   <link rel="canonical" href="https://www.clinicafcare.com.br/" />
 </svelte:head>
 
