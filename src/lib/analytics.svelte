75f37483d7093ef1300482fa70578d3fc3bb41f2
Daniel Faustino <danieloliveirafaustino@gmail.com>
2022-05-25 23:56:56 -0300

    Gtags js

---
+++ src/lib/analytics.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { page } from '$app/stores';
+
+	let MEASUREMENT_ID = 'G-52CE4NWL3F';
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', MEASUREMENT_ID, {
+				page_title: document.title,
+				page_path: $page.url.href
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}>
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', MEASUREMENT_ID);
+	</script>
+</svelte:head>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,10 @@
   import "../app.css";
   import Nav from '../components/nav.svelte'
   import Footer from '../components/footer.svelte';
+  import Analytics from '$lib/analytics.svelte'
   </script>
+
+<Analytics />
 <Nav/>
 <div class="w-full h-screen mt-24">
 <slot></slot>
