6856386111894f5dd140fd15489abe02ff493bc1
Daniel Faustino <danieloliveirafaustino@gmail.com>
2022-05-25 23:07:50 -0300

    svelte seo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
 	import InstagramCarousel from '../components/instagramCarousel.svelte';
 	import EquipeBanner from '../components/equipeBanner.svelte';
 	import SobreBanner from '../components/sobreBanner.svelte';
+  import SvelteSeo from  'svelte-seo';
 </script>
 
 <svelte:head>
@@ -10,6 +11,23 @@
   <meta name="googlebot" content="Extensão de Cílios, Epilação Egípcia, Estética, Peeling, Podologia, Despigmentação a Laser, Design de Sobrancelhas, Revitalização Labial, Micropigmentação, Extensão de Cílios, Cursos,Botox,Preenchimento Labial,Fabrine Macedo" />
   <link rel="canonical" href="https://www.clinicafcare.com.br/" />
 </svelte:head>
+
+<SvelteSeo
+  openGraph={{
+    title: 'Clínica F.Care',
+    description: 'Extensão de Cílios, Epilação Egípcia, Estética, Peeling, Podologia, Despigmentação a Laser, Design de Sobrancelhas, Revitalização Labial, Micropigmentação, Extensão de Cílios, Cursos,Botox,Preenchimento Labial,Fabrine Macedo',
+    url: 'https://www.clinicafcare.com.br',
+    type: 'website',
+    images: [
+      {
+        url: 'about.jpg',
+        width: 850,
+        height: 650,
+        alt: 'Clínica F.Care'
+      }
+     ]
+  }}
+/>
 
 <EquipeBanner />
 <div class="py-8">
