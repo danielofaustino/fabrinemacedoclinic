17a013c7d3e1fcb3f58ada8af08dfbd179df3c0b
Daniel Faustino <danieloliveirafaustino@gmail.com>
2022-10-21 09:50:10 -0300

    fix: equipe cleanup

--- src/components/equipeBanner.svelte
+++ src/components/equipeBanner.svelte
@@ -1,7 +1,7 @@
 <section class="relative bg-white">
 	<img
 		class="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-25 sm:opacity-100"
-		src="equipe/equipe.webp"
+		src="about.webp"
 		alt="Nossa Equipe"
 	/>
 
--- src/stores/equipe.mjs
+++ src/stores/equipe.mjs
@@ -5,11 +5,7 @@ export const equipeData = [
 
   { id: '2', nome: 'Amanda Dantas', foto: "/equipe/amanda.webp", especialidade: "Podologia", procedimentos: [{ nome: 'Design Estratégico', description: 'Estamos atualizando os textos, entre em contato no whatsApp e veja o nosso catalogo:', fotos: [] }, { nome: 'Brows Lamination', description: 'Estamos atualizando os textos, entre em contato no whatsApp e veja o nosso catalogo:', fotos: [] }] },
 
-  { id: '3', nome: 'Juliana Marchezetti', foto: "/equipe/juliana.webp", especialidade: "Despigmentação à Laser", procedimentos: [{ nome: 'Design Estratégico', description: 'Estamos atualizando os textos, entre em contato no whatsApp e veja o nosso catalogo:', fotos: [] }, { nome: 'Brows Lamination', description: 'Estamos atualizando os textos, entre em contato no whatsApp e veja o nosso catalogo:', fotos: [] }] },
-
   { id: '4', nome: 'Grascielly Medeiros', foto: "/equipe/grascielly.webp", especialidade: "Harmonização Facial", procedimentos: [{ nome: 'Design Estratégico', description: 'Estamos atualizando os textos, entre em contato no whatsApp e veja o nosso catalogo:', fotos: [] }, { nome: 'Brows Lamination', description: 'Estamos atualizando os textos, entre em contato no whatsApp e veja o nosso catalogo:', fotos: [] }] },
-
-  { id: '5', nome: 'Karen Dantas', foto: "/equipe/karen.webp", especialidade: "Extensão de Cílios", procedimentos: [{ nome: 'Design Estratégico', description: 'Estamos atualizando os textos, entre em contato no whatsApp e veja o nosso catalogo:', fotos: [] }, { nome: 'Brows Lamination', description: 'Estamos atualizando os textos, entre em contato no whatsApp e veja o nosso catalogo:', fotos: [] }] },
 
   { id: '6', nome: 'Jessica Galera', foto: "/equipe/jessica.webp", especialidade: "Estética", procedimentos: [{ nome: 'Design Estratégico', description: 'Estamos atualizando os textos, entre em contato no whatsApp e veja o nosso catalogo:', fotos: [] }, { nome: 'Brows Lamination', description: 'Estamos atualizando os textos, entre em contato no whatsApp e veja o nosso catalogo:', fotos: [] }] },
 ]
