a1392dcea453ede6d15f83821751fc214ad363bc
Daniel Faustino <danielofaustino@proton.me>
2023-06-12 21:46:32 -0300

    refactor: facebook pixel

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,37 @@
 <script>
-  import "../app.css";
-  import Nav from '../components/nav.svelte'
-  import Footer from '../components/footer.svelte';
-  import Analytics from '$lib/analytics.svelte'
-  </script>
+	import '../app.css';
+	import Nav from '../components/nav.svelte';
+	import Footer from '../components/footer.svelte';
+	import Analytics from '$lib/analytics.svelte';
+	export const segments = [];
+
+	$: {
+		if (typeof window !== 'undefined') {
+			!(function (f, b, e, v, n, t, s) {
+				if (f.fbq) return;
+				n = f.fbq = function () {
+					n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+				};
+				if (!f._fbq) f._fbq = n;
+				n.push = n;
+				n.loaded = !0;
+				n.version = '2.0';
+				n.queue = [];
+				t = b.createElement(e);
+				t.async = !0;
+				t.src = v;
+				s = b.getElementsByTagName(e)[0];
+				s.parentNode.insertBefore(t, s);
+			})(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+			fbq('init', '1605057826682236');
+			fbq('track', 'PageView');
+		}
+	}
+</script>
 
 <Analytics />
-<Nav/>
+<Nav />
 <div class="w-full h-screen mt-24">
-<slot></slot>
-<Footer />
+	<slot />
+	<Footer />
 </div>
-
-
-
